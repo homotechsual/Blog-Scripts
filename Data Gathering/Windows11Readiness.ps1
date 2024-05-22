1cb51a186023608e333fbc5d89358fae4f5f3b9e
Mikey O'Toole <mikey@homotechsual.dev>
2024-05-22 09:14:27 +0100

    Update Windows 11 readiness script.

--- Data Gathering/Windows11Readiness.ps1
+++ Data Gathering/Windows11Readiness.ps1
@@ -4,6 +4,7 @@
     .DESCRIPTION
         This script will check the current system for Windows 11 readiness. It will check the following: OS Disk Size, Memory Capacity, CPU Clock Speed, CPU Logical Processors, CPU Address Width, CPU Family Type, TPM Version, Secure Boot, and UEFI Firmware.
     .NOTES
+        2024-05-22: Add table conversion function to convert results to HTML table for Ninja WYSIWYG field. Fix TPM version check to use maximum version. Thanks to Fly Kick and Mark aka AIVenom aka NinjaOne's Technical Product Manager for Scripting for the feedback, suggestions and code corrections / improvements. Thanks to Skolte for the table conversion function.
         2024-04-15: Update for changed Windows 11 requirements.
         2023-03-26: Fix incorrect variable comparison bugs. Reformat.
         2022-02-16: Fix bug with Secure Boot suitability check, add i7-7820hq on Surface Studio 2 or Precision 5520 as supported CPU outside of CPU family check.
@@ -13,6 +14,47 @@
     .LINK
         Blog post: https://homotechsual.dev/2022/12/22/NinjaOne-custom-fields-endless-possibilities/
 #>
+
+
+## Custom Function To Convert Results To Table So We Can Use Ninja WYSIWYG Field
+function ConvertTo-ObjectToHtmlTable {
+    param (
+        [Parameter(Mandatory = $true)]
+        [System.Collections.Generic.List[Object]]$Objects
+    )
+
+    $sb = New-Object System.Text.StringBuilder
+
+    # Start the HTML table
+    [void]$sb.Append('<table><thead><tr>')
+
+    # Add column headers based on the properties of the first object, excluding "RowColour"
+    $Objects[0].PSObject.Properties.Name | Where-Object { $_ -ne 'RowColour' } | ForEach-Object { [void]$sb.Append("<th>$_</th>") }
+
+    [void]$sb.Append('</tr></thead><tbody>')
+
+    foreach ($obj in $Objects) {
+        # Use the RowColour property from the object to set the class for the row
+        $rowClass = if ($obj.RowColour) { $obj.RowColour } else { '' }
+
+        [void]$sb.Append("<tr class=`"$rowClass`">")
+        # Generate table cells, excluding "RowColour"
+        foreach ($propName in $obj.PSObject.Properties.Name | Where-Object { $_ -ne 'RowColour' }) {
+            [void]$sb.Append("<td>$($obj.$propName)</td>")
+        }
+        [void]$sb.Append('</tr>')
+    }
+
+    [void]$sb.Append('</tbody></table>')
+
+    return $sb.ToString()
+}
+
+
+## Main Script Block ##
+
+$Win11ReadinessResults = New-Object 'System.Collections.Generic.List[Object]'
+
 try {
     [int]$MinOSDiskSizeGB = 64
     [int]$MinMemoryGB = 4
@@ -158,7 +200,7 @@ public class CpuFamily
 }
 '@
     $OSDriveSize = Get-CimInstance -Class Win32_LogicalDisk -Filter "DeviceID='$($ENV:SystemDrive)'" | Select-Object @{ 
-        Name = 'SizeGB'
+        Name       = 'SizeGB'
         Expression = { $_.Size / 1GB -as [int] }
     }
     if ($OSDriveSize.SizeGB -ge $MinOSDiskSizeGB) {
@@ -167,7 +209,7 @@ public class CpuFamily
         $OSDriveSizeSuitable = 'No'
     }
     $MemorySize = Get-CimInstance -Class Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum | Select-Object @{
-        Name = 'SizeGB'
+        Name       = 'SizeGB'
         Expression = { $_.Sum / 1GB -as [int] }
     }
     if ($MemorySize.SizeGB -ge $MinMemoryGB) {
@@ -178,7 +220,8 @@ public class CpuFamily
     $TPM = Get-Tpm
     if ($TPM.TpmPresent) {
         $TPMVersion = Get-CimInstance -Class Win32_Tpm -Namespace 'root\CIMV2\Security\MicrosoftTpm' | Select-Object -Property SpecVersion
-        $TPMMajorVersion = $TPMVersion.SpecVersion.Split(',')[0]
+        $TPMMajorVersion = ($TPMVersion.SpecVersion.Split(',').Trim() | ForEach-Object { [decimal]$_ } | Measure-Object -Maximum).Maximum
+        
         if ($TPMMajorVersion -ge 2) {
             $TPMSuitable = 'Yes'
         } else {
@@ -240,22 +283,91 @@ public class CpuFamily
     }
     if ($OSDriveSizeSuitable -eq 'Yes' -and $MemorySizeSuitable -eq 'Yes' -and $CPUSuitable -eq 'Yes' -and $TPMSuitable -eq 'Yes' -and $SecureBootSuitable -eq 'Yes') {
         Ninja-Property-Set windows11Capable 1 | Out-Null
+        Write-Host 'Device Is Windows 11 Suitable'
     } else {
         Ninja-Property-Set windows11Capable 0 | Out-Null
+        Write-Host 'Device Is Not Windows 11 Suitable'
     }
-    $Windows11CheckDetails = @{
-        'OSDriveSizeSuitable' = $OSDriveSizeSuitable
-        'OSDriveSize' = $OSDriveSize.SizeGB
-        'MemorySizeSuitable' = $MemorySizeSuitable
-        'MemorySize' = $MemorySize.SizeGB
-        'CPUSuitable' = $CPUSuitable
-        'CPUFamily' = $ProcessorFamily
-        'TPMSuitable' = $TPMSuitable
-        'SecureBootSuitable' = $SecureBootSuitable
-        'SecureBootPossible' = $SecureBootPossible
+    ## Create List Of Results So We Can Convert To Table & Write To Ninja WYSIWYG Field
+    $CPUFamilyRow = [PSCustomObject]@{
+        'Check'     = 'CPUFamily'
+        'Result'    = $ProcessorFamily
+        'RowColour' = if ($CPUSuitable -eq 'Yes') { 'success' }else { 'danger' }
     }
-    $DetailJson = $Windows11CheckDetails | ConvertTo-Json
-    Ninja-Property-Set windows11CheckDetails $DetailJson | Out-Null
+    $Win11ReadinessResults.Add($CPUFamilyRow)
+
+    $CPUSuitableRow = [PSCustomObject]@{
+        'Check'     = 'CPUSuitable'
+        'Result'    = $CPUSuitable
+        'RowColour' = if ($CPUSuitable -eq 'Yes') { 'success' }else { 'danger' }
+    }
+    $Win11ReadinessResults.Add($CPUSuitableRow)
+
+    $MemorySizeRow = [PSCustomObject]@{
+        'Check'     = 'MemorySizeGB'
+        'Result'    = $MemorySize.SizeGB
+        'RowColour' = if ($MemorySizeSuitable -eq 'Yes') { 'success' }else { 'danger' }
+
+    }
+    $Win11ReadinessResults.Add($MemorySizeRow)
+
+    $MemorySizeSuitableRow = [PSCustomObject]@{
+        'Check'     = 'MemorySizeSuitable'
+        'Result'    = $MemorySizeSuitable
+        'RowColour' = if ($MemorySizeSuitable -eq 'Yes') { 'success' }else { 'danger' }
+    }
+    $Win11ReadinessResults.Add($MemorySizeSuitableRow)
+
+    $OSDriveSizeRow = [PSCustomObject]@{
+        'Check'     = 'OSDriveSizeGB'
+        'Result'    = $OSDriveSize.SizeGB
+        'RowColour' = if ($OSDriveSizeSuitable -eq 'Yes') { 'success' } else { 'danger' }
+    }
+    $Win11ReadinessResults.Add($OSDriveSizeRow)
+
+    $OSDriveSizeSuitableRow = [PSCustomObject]@{
+        'Check'     = 'OSDriveSizeSuitable'
+        'Result'    = $OSDriveSizeSuitable
+        'RowColour' = if ($OSDriveSizeSuitable -eq 'Yes') { 'success' } else { 'danger' }
+    }
+    $Win11ReadinessResults.Add($OSDriveSizeSuitableRow)
+
+    $TPMVerRow = [PSCustomObject]@{
+        'Check'     = 'TPM Versions'
+        'Result'    = $TPMVersion.SpecVersion
+        'RowColour' = if ($TPMSuitable -eq 'Yes') { 'success' } else { 'danger' }
+    }
+    $Win11ReadinessResults.Add($TPMVerRow)
+
+    $TPMSuitableRow = [PSCustomObject]@{
+        'Check'     = 'TPMSuitable'
+        'Result'    = $TPMSuitable
+        'RowColour' = if ($TPMSuitable -eq 'Yes') { 'success' } else { 'danger' }
+    }
+    $Win11ReadinessResults.Add($TPMSuitableRow)
+
+    $SecureBootSuitableRow = [PSCustomObject]@{
+        'Check'     = 'SecureBootSuitable'
+        'Result'    = $SecureBootSuitable
+        'RowColour' = if ($SecureBootSuitableRow -eq 'Yes') { 'success' } else { 'danger' }
+    }
+    $Win11ReadinessResults.Add($SecureBootSuitableRow)
+
+    $SecureBootPossibleRow = [PSCustomObject]@{
+        'Check'     = 'SecureBootPossible'
+        'Result'    = $SecureBootPossible
+        'RowColour' = if ($SecureBootPossibleRow -eq 'Yes') { 'success' } else { 'danger' }
+    }
+    $Win11ReadinessResults.Add($SecureBootPossibleRow)
+
+    ## Convert List Of Results To HTML Table
+    $Win11ReadinessTbl = ConvertTo-ObjectToHtmlTable -Objects $Win11ReadinessResults
+
+    ## Write Results To Ninja WYSIWYG Custom Field
+    Ninja-Property-Set windows11Readiness $Win11ReadinessTbl | Out-Null
+
+    ## Write Results Out To Script Log
+    $Win11ReadinessResults
 } catch {
     Write-Error $_
 }
