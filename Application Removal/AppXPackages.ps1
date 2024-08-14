c588d698e01a507e35eb6f2868e7027b735aab05
Mikey O'Toole <mikey@homotechsual.dev>
2024-08-14 22:51:14 +0100

    Add AppX Package removal script.

---
+++ Application Removal/AppXPackages.ps1
@@ -0,0 +1,118 @@
+<#
+    .SYNOPSIS
+        Application Removal - Windows - AppX Packages
+    .DESCRIPTION
+        This script is used to remove AppX packages from a Windows machine. The script will attempt to remove the packages for the current user and all users. If the packages are provisioned, it will also deprovision the packages. Accepts input from a NinjaOne script variable named "PackageNames" which should be a comma separated list of package names.
+
+        Use a checkbox script variable to allow wildcard / partial matches. This should be named "AllowWildcards".
+    .NOTES
+        2024-08-14: Initial version
+    .LINK
+        Blog post: Not blogged yet
+#>
+# Utility Function: AppPackage.RemoveandDeprovision
+## This function is used to ensure that an app package is uninstalled for all users and deprovisioned.
+function AppPackage.RemoveandDeprovision {
+    [CmdletBinding()]
+    param(
+        # An array of package names to remove.
+        [Parameter(Mandatory)]
+        [string[]]$PackageNames,
+        # Allow wildcard / partial matches.
+        [switch]$AllowWildcards
+    )
+    $exitvalue = 0
+    foreach ($PackageName in $PackageNames) {
+        Write-Host ('Checking if package {0} is installed...' -f $PackageName)
+        # Find the provisioned package
+        $ProvisionedPackages = Get-AppxProvisionedPackage -Online
+        $ProvisionedPackage = if ($AllowWildcards) {
+            $ProvisionedPackages | Where-Object { $_.DisplayName -like ('*{0}*' -f $PackageName) }
+        } else {
+            $ProvisionedPackages | Where-Object { $_.DisplayName -eq $PackageName }
+        }
+        $PackageRemovalLoopCount = 0
+        # Try current user remove.
+        do {
+            Write-Host ('Attempt {0} to find and remove package {1}.' -f $PackageRemovalLoopCount, $PackageName)
+            $PackageRemovalLoopCount++
+            $Package = if ($AllowWildcards) {
+                Get-AppxPackage | Where-Object { $_.Name -like ('*{0}*' -f $PackageName) }
+            } else {
+                Get-AppxPackage -Name $PackageName
+            }
+            if ($Package) {
+                try {
+                    # Uninstall the app package
+                    $null = $Package | Remove-AppxPackage
+                    Start-Sleep -Seconds 5
+                    Write-Host ('Package {0} has been successfully uninstalled.' -f $PackageName)
+                } catch {
+                    Write-Error ('Failed to uninstall package {0}. Error: {1}' -f $PackageName, $_)
+                    $exitvalue++
+                }
+            } else {
+                Write-Host ('Package {0} is not installed or not found. Skipping uninstallation.' -f $PackageName)
+            }
+        } until (($null -eq $Package) -or $PackageRemovalLoopCount -eq 3)
+        # Try all users remove.
+        do {
+            Write-Host ('Attempt {0} to find and remove package {1}.' -f $PackageRemovalLoopCount, $PackageName)
+            $PackageRemovalLoopCount++
+            $Package = if ($AllowWildcards) {
+                Get-AppxPackage -AllUsers | Where-Object { $_.Name -like ('*{0}*' -f $PackageName) }
+            } else {
+                Get-AppxPackage -AllUsers -Name $PackageName
+            }
+            if ($Package) {
+                try {
+                    # Uninstall the app package
+                    $null = $Package | Remove-AppxPackage -AllUsers
+                    Start-Sleep -Seconds 5
+                    Write-Host ('Package {0} has been successfully uninstalled.' -f $PackageName)
+                } catch {
+                    Write-Error ('Failed to uninstall package {0}. Error: {1}' -f $PackageName, $_)
+                    $exitvalue++
+                }
+            } else {
+                Write-Host ('Package {0} is not installed or not found. Skipping uninstallation.' -f $PackageName)
+            }
+        } until (($null -eq $Package) -or $PackageRemovalLoopCount -eq 3)
+        $Package = if ($AllowWildcards) {
+            Get-AppxPackage -AllUsers | Where-Object { $_.Name -like ('*{0}*' -f $PackageName) }
+        } else {
+            Get-AppxPackage -AllUsers -Name $PackageName
+        }
+        if ($Package) {
+            Write-Error ('Failed to uninstall package {0} after 3 attempts. Please see the error messages above.' -f $PackageName)
+            $exitvalue++
+        }
+        if ($ProvisionedPackage) {
+            try {
+                # Deprovision the app package
+                $null = $ProvisionedPackage | Remove-AppxProvisionedPackage -AllUsers -Online
+                Write-Host ('Package {0} has been deprovisioned.' -f $PackageName)
+            } catch {
+                Write-Error ('Failed to deprovision app package {0}. Error: {1}' -f $PackageName, $_)
+                $exitvalue++
+            }
+        } else {
+            Write-Host ('Package {0} is not provisoned. Skipping deprovisioning.' -f $PackageName)
+        }
+    }
+    return $exitvalue
+}
+if ([string]::IsNullOrWhiteSpace($ENV:PackageNames)) {
+    throw 'No package names were found. Please ensure your script variable has the machine name "PackageNames" and contains a comma separated list of package names.'
+}
+if ([string]::IsNullOrWhiteSpace($ENV:AllowWildcards)) {
+    $AllowWildcards = $false
+} else {
+    $AllowWildcards = [boolean]::Parse($ENV:AllowWildcards)
+}
+$PackageNames = ($ENV:PackageNames.Split(',').Trim())
+if ($PackageNames.Count -ge 1) {
+    AppPackage.RemoveandDeprovision -PackageNames $PackageNames -AllowWildcards:$AllowWildcards
+} else {
+    throw 'No package names were found. Please ensure your script variable has the machine name "PackageNames" and contains a comma separated list of package names.'
+}
